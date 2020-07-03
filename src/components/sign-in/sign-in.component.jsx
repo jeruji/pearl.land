65dae984878a083935a47c87dcf992ff4d2d557e
echo <lonelybutts@gmail.com>
2020-07-04 06:28:44 +0700

    updating our app to redirect to home if user signed in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -50,7 +50,7 @@ class SignIn extends React.Component {
 
   handleSubmit = async (event) => {
     event.preventDefault();
-
+    this.validateForm();
     const { email, password } = this.state;
 
     try {
@@ -92,7 +92,11 @@ class SignIn extends React.Component {
           />
           <div className="buttons">
             <CustomButton type="submit">Sign in</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type="button"
+              onClick={signInWithGoogle}
+              isGoogleSignIn
+            >
               {" "}
               Sign in with Google{" "}
             </CustomButton>
